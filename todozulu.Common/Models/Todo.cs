492d84b04e8b3873248e780a37614497b780b586
Juan Carlos Zuluaga <jzuluaga55@gmail.com>
2021-08-15 20:26:19 -0500

    Add entity

---
+++ todozulu.Common/Models/Todo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace todozulu.Common.Models
+{
+    public class Todo
+    {
+        public DateTime CreatedTime { get; set; }
+
+        public string TaskDescription { get; set; }
+
+        public bool IsCompleted { get; set; }
+    }
+}
---
+++ todozulu.Functions/Entities/TodoEntity.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+using System;
+
+namespace todozulu.Functions.Entities
+{
+    public class TodoEntity : TableEntity
+    {
+        public DateTime CreatedTime { get; set; }
+
+        public string TaskDescription { get; set; }
+
+        public bool IsCompleted { get; set; }
+    }
+}
